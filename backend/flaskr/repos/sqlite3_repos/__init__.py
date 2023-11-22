d6e03b765c5c51733adc0817bcae06edcd61cd32
Phan Lam Tung <lamtungpro1405@gmail.com>
2023-11-22 17:03:39 +0700

    add constant

---
+++ backend/flaskr/repos/sqlite3_repos/__init__.py
@@ -0,0 +1,17 @@
+MODERATOR_TABLE_NAME = "moderators"
+MODERATOR_TYPE_NAME = "moderator"
+
+GROUP_TABLE_NAME = "groups"
+GROUP_TYPE_NAME = "group"
+
+MEMBER_TABLE_NAME = "members"
+MEMBER_TYPE_NAME = "member"
+
+USER_TABLE_NAME = "users"
+USER_TYPE_NAME = "user"
+
+COLLECT_SESSION_TABLE_NAME = "collect_sessions"
+COLLECT_SESSION_TYPE_NAME = "collect_session"
+
+COLLECT_SESSION_ENTRY_TABLE_NAME = "collect_session_entries"
+COLLECT_SESSION_ENTRY_TYPE_NAME = "collect_session_entry"

00149759029c3f79d8e0d7e36ef90e2f246f8d1d
ltp1405 <lamtungphan1405@gmail.com>
2023-11-22 21:38:19 +0700

    update interface of group repo

--- backend/flaskr/repos/interfaces/group_repo.py
+++ backend/flaskr/repos/interfaces/group_repo.py
@@ -2,45 +2,42 @@ from abc import ABC, abstractmethod
 from typing import Dict, List, Tuple
 
 from flaskr.repos.datatypes.member import Member
+from flaskr.repos.datatypes.moderator import Moderator
 from ..datatypes.group import Group
 from ..datatypes.user import User
 
 
 class AbstractGroupRepo(ABC):
     @abstractmethod
-    def get_group(self, group_id: int) -> Group | None:
+    def get_group(self, group_name: str, group_leader: str) -> Group | None:
         pass
 
     @abstractmethod
-    def get_groups(self) -> List[Group]:
+    def get_all_groups(self, max: int) -> List[Group]:
         pass
 
     @abstractmethod
-    def add(self, group: Group) -> Group:
+    def add(self, group: Group):
         pass
 
     @abstractmethod
-    def update(self, group_id: int, group: Group) -> Group:
+    def update(self, group: Group):
         pass
 
     @abstractmethod
-    def delete(self, group_id: int) -> bool:
+    def delete(self, group_id: int):
         pass
 
     @abstractmethod
-    def get_members(
-        self, group_name: str, group_leader: str
-    ) -> Tuple[Group, List[User]] | None:
+    def get_members(self, group_name: str, group_leader: str) -> List[Member] | None:
         pass
 
     @abstractmethod
-    def get_moderators(
-        self, group_name: str, group_leader: str
-    ) -> Dict[Group, List[User]]:
+    def get_moderators(self, group_name: str, group_leader: str) -> List[User]:
         pass
 
     @abstractmethod
-    def get_groups_and_members(self) -> Dict[Group, List[Member]]:
+    def get_groups_and_members(self, max: int) -> Dict[Group, List[Member]]:
         pass
 
     @abstractmethod
@@ -48,21 +45,17 @@ class AbstractGroupRepo(ABC):
         pass
 
     @abstractmethod
-    def add_member(self, member: Member) -> bool:
+    def add_member(self, member: Member):
         pass
 
     @abstractmethod
-    def remove_member(self, group_name: str, group_leader: str, member: Member) -> bool:
+    def remove_member(self, member: Member):
         pass
 
     @abstractmethod
-    def add_moderator(
-        self, group_name: str, group_leader: str, moderator: User
-    ) -> bool:
+    def add_moderator(self, moderator: Moderator):
         pass
 
     @abstractmethod
-    def remove_moderator(
-        self, group_name: str, group_leader: str, moderator: User
-    ) -> bool:
+    def remove_moderator(self, moderator: Moderator):
         pass
